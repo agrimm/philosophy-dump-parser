155cb73654c0f61029eb1e9c0bd2c490f47d6a3e
Andrew Grimm <andrew.j.grimm@gmail.com>
2008-11-22 20:37:37 +1100

    Removed hard-wired references to the XML structure.

--- lib/page_xml_parser.rb
+++ lib/page_xml_parser.rb
@@ -11,16 +11,11 @@ class PageXmlParser
     xml = REXML::Document.new(@page_xml_file)
     pages = []
 
-    #To do: replace these hard-wired numbers
-    2.upto(xml.root.elements.size) do |i|
-      page_element = xml.root.elements[i]
-      title_element = page_element.elements[1]
+    xml.elements.each("*/page") do |page_element|
+      title_element = page_element.elements["title"]
       title = title_element.text
 
-      j = page_element.elements.size
-      revision_element = page_element.elements[j]
-      k = revision_element.elements.size
-      text_element = revision_element.elements[k]
+      text_element = page_element.elements["revision/text"]
       text = text_element.text
 
       page = Page.new_if_valid(title, text)

eb228558013e2f72e7c11b3dbe99cc640b3e5ca9
Andrew Grimm <andrew.j.grimm@gmail.com>
2008-11-29 18:46:26 +1100

    Changed XML element searching for optimization purposes.

--- lib/page_xml_parser.rb
+++ lib/page_xml_parser.rb
@@ -11,17 +11,23 @@ class PageXmlParser
     xml = XML::Document.string(@page_xml_file.read)
     pages = []
 
-    xml.root.find("*[local-name()='page']").each do |page_element|
+    xml.root.find("*").each do |page_element|
+      next if page_element.name == "siteinfo"
       title, text = nil, nil
-
-      page_element.find("*[local-name()='title']").each do |title_element|
-        title = String(title_element.content)
+      page_element.find("*").each do |page_element_child|
+        if page_element_child.name == "title"
+          title_element = page_element_child
+          title = String(title_element.content)
+        elsif page_element_child.name == "revision"
+          revision_element = page_element_child
+          revision_element.find("*").each do |revision_element_child|
+            next unless revision_element_child.name == "text"
+            text_element = revision_element_child
+            text = String(text_element.content)
+          end
+        end
       end
-
-      page_element.find("*/*[local-name()='text']").each do |text_element|
-        text = String(text_element.content)
-      end
-
+      raise if title.nil? or text.nil?
       page = Page.new_if_valid(title, text)
       pages << page unless page.nil?
     end

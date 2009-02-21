9588e5920b8fcacb6a784fb7216523dc8f672ad2
Andrew Grimm <andrew.j.grimm@gmail.com>
2009-02-21 21:31:54 +1100

    Added monkeypatch for testing from stack overflow.

--- test/ts_unit.rb
+++ test/ts_unit.rb
@@ -1,6 +1,26 @@
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 $:.unshift File.join(File.dirname(__FILE__), "..", "test")
 
+require "test/unit"
+
+class Test::Unit::TestCase
+
+  def self.method_added(sym)
+    raise "#{sym} already defined!" if my_tests.include? sym
+    my_tests_add sym
+  end
+
+  def self.my_tests_add(test)
+    @my_tests << test
+  end
+
+  def self.my_tests
+    @my_tests ||= []
+  end
+
+end
+
+
 require "tc_page_xml_parsing"
 require "tc_page"
 require "tc_repository"
